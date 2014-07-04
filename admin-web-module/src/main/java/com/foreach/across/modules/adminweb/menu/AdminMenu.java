d604b2e600f9e060827b5b827be92c80c04007d9
Arne Vandamme <arne@foreach.be>
2014-07-04 17:24:17 +0200

    Migrate standard modules to separate repository

---
+++ admin-web-module/src/main/java/com/foreach/across/modules/adminweb/menu/AdminMenu.java
@@ -0,0 +1,12 @@
+package com.foreach.across.modules.adminweb.menu;
+
+import com.foreach.across.modules.web.menu.Menu;
+
+public class AdminMenu extends Menu
+{
+	public static final String NAME = "adminMenu";
+
+	public AdminMenu() {
+		super( NAME );
+	}
+}
