5bccd09dbd877b04c3fbef11238726b22d8c4867
Marc Vanbrabant <marc.vanbrabant@mediafin.be>
2014-07-29 14:50:51 +0200

    Utility class to check authorities

---
+++ spring-security/src/main/java/com/foreach/across/modules/spring/security/AuthenticationUtils.java
@@ -0,0 +1,26 @@
+package com.foreach.across.modules.spring.security;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.util.CollectionUtils;
+
+import java.util.Collection;
+
+public final class AuthenticationUtils
+{
+	private AuthenticationUtils() {}
+	public static boolean hasAuthority( Authentication authentication, String authority ) {
+		if( authentication != null ) {
+			Collection<? extends GrantedAuthority> grantedAuthorities = authentication.getAuthorities();
+			if( !CollectionUtils.isEmpty( grantedAuthorities ) ) {
+				for( GrantedAuthority grantedAuthority : grantedAuthorities ) {
+					if( grantedAuthority != null && StringUtils.equals( grantedAuthority.getAuthority(), authority ) ) {
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+}
---
+++ spring-security/src/test/java/TestAuthenticationUtils.java
@@ -0,0 +1,42 @@
+import com.foreach.across.modules.spring.security.AuthenticationUtils;
+import org.junit.Test;
+import org.mockito.internal.util.collections.Sets;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class TestAuthenticationUtils
+{
+	@Test
+	public void testInvalidAuthorityReturnsFalse() throws Exception {
+		assertEquals( false, AuthenticationUtils.hasAuthority( null, "foezoj" ) );
+	}
+
+	@Test
+	public void testAuthorityWithNullAuthoritiesReturnsFalse() throws Exception {
+		assertEquals( false, AuthenticationUtils.hasAuthority( mock( Authentication.class ), "foezoj" ) );
+	}
+
+	@Test
+	@SuppressWarnings( "unchecked" )
+	public void testAuthorityWithAuthoritiesAndNullAuthorityReturnsFalse() throws Exception {
+		Authentication authentication = mock( Authentication.class );
+		Collection grantedAuthorities = Sets.newSet( null, new SimpleGrantedAuthority( "bla" ) );
+		when( authentication.getAuthorities() ).thenReturn( grantedAuthorities );
+		assertEquals( false, AuthenticationUtils.hasAuthority( authentication, null ) );
+	}
+
+	@Test
+	@SuppressWarnings( "unchecked" )
+	public void testAuthorityWithAuthoritiesAndNullAuthorityReturnsTrue() throws Exception {
+		Authentication authentication = mock( Authentication.class );
+		Collection grantedAuthorities = Sets.newSet( null, new SimpleGrantedAuthority( "bla" ) );
+		when( authentication.getAuthorities() ).thenReturn( grantedAuthorities );
+		assertEquals( true, AuthenticationUtils.hasAuthority( authentication, "bla" ) );
+	}
+}
