4dd49fc2b6996cbbf1c0e1c24b41f65e8de2ab63
Arne Vandamme <arne@foreach.be>
2014-09-01 14:32:09 +0200

    Update settings classes

--- admin-web-module/src/main/java/com/foreach/across/modules/adminweb/AdminWeb.java
+++ admin-web-module/src/main/java/com/foreach/across/modules/adminweb/AdminWeb.java
@@ -1,9 +1,7 @@
 package com.foreach.across.modules.adminweb;
 
 import com.foreach.across.modules.web.context.PrefixingPathContext;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 
 public final class AdminWeb extends PrefixingPathContext
 {
@@ -13,31 +11,21 @@ public final class AdminWeb extends PrefixingPathContext
 	public static final String LAYOUT_TEMPLATE = "th/adminweb/layouts/adminPage";
 
 	@Autowired
-	private Environment environment;
+	private AdminWebModuleSettings settings;
 
 	private final String title;
-	private final AdminWebSettings settings;
 
 	public AdminWeb( String prefix, String title ) {
 		super( prefix );
 
 		this.title = title;
-		this.settings = new AdminWebSettings();
-
 	}
 
 	public String getTitle() {
 		return title;
 	}
 
-	public AdminWebSettings getSettings() {
+	public AdminWebModuleSettings getSettings() {
 		return settings;
-	}
-
-	public class AdminWebSettings
-	{
-		public boolean isRememberMeEnabled() {
-			return !StringUtils.isBlank( environment.getProperty( AdminWebModuleSettings.REMEMBER_ME_KEY, "" ) );
-		}
 	}
 }
--- admin-web-module/src/main/java/com/foreach/across/modules/adminweb/AdminWebModuleSettings.java
+++ admin-web-module/src/main/java/com/foreach/across/modules/adminweb/AdminWebModuleSettings.java
@@ -1,6 +1,10 @@
 package com.foreach.across.modules.adminweb;
 
-public class AdminWebModuleSettings
+import com.foreach.across.core.AcrossModuleSettings;
+import com.foreach.across.core.AcrossModuleSettingsRegistry;
+import org.apache.commons.lang3.StringUtils;
+
+public class AdminWebModuleSettings extends AcrossModuleSettings
 {
 	/**
 	 * URL or relative path (without admin prefix) for the page to which a user will be sent
@@ -32,6 +36,16 @@ public class AdminWebModuleSettings
 	public static final String REMEMBER_ME_TOKEN_VALIDITY_SECONDS =
 			"adminWebModule.login.rememberMe.tokenValiditySeconds";
 
-	protected AdminWebModuleSettings() {
+	@Override
+	protected void registerSettings( AcrossModuleSettingsRegistry registry ) {
+		registry.register( REMEMBER_ME_KEY, String.class, null,
+		                   "Key used for creating the remember me cookie.  " +
+				                   "If this property is not present, remember me will not be enabled." );
+		registry.register( REMEMBER_ME_TOKEN_VALIDITY_SECONDS, Integer.class, 2592000,
+		                   "Number of seconds a remember me token should be valid. Defaults to 30 days." );
+	}
+
+	public boolean isRememberMeEnabled() {
+		return !StringUtils.isBlank( getProperty( AdminWebModuleSettings.REMEMBER_ME_KEY, "" ) );
 	}
 }
