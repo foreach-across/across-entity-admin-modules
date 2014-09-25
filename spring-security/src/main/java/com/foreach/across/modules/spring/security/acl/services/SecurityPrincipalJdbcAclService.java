a19af2c845e804af90db95fe8e8ad995f8eec9ab
Marc Vanbrabant <marc.vanbrabant@mediafin.be>
2014-09-25 12:13:00 +0200

    Cache calls to createOrRetrieveSidPrimaryKey and createOrRetrieveClassPrimaryKey

--- spring-security/src/main/java/com/foreach/across/modules/spring/security/acl/services/SecurityPrincipalJdbcAclService.java
+++ spring-security/src/main/java/com/foreach/across/modules/spring/security/acl/services/SecurityPrincipalJdbcAclService.java
@@ -15,10 +15,16 @@
  */
 package com.foreach.across.modules.spring.security.acl.services;
 
+import com.foreach.across.modules.spring.security.acl.config.AclSecurityConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
 import org.springframework.security.acls.jdbc.JdbcMutableAclService;
 import org.springframework.security.acls.jdbc.LookupStrategy;
-import org.springframework.security.acls.model.*;
+import org.springframework.security.acls.model.AclCache;
+import org.springframework.security.acls.model.Sid;
 
+import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 
 /**
@@ -26,9 +32,55 @@ import javax.sql.DataSource;
  */
 public class SecurityPrincipalJdbcAclService extends JdbcMutableAclService
 {
+	@Autowired( required = false )
+	private CacheManager cacheManager;
+
+	private Cache cache = null;
+
 	public SecurityPrincipalJdbcAclService( DataSource dataSource,
 	                                        LookupStrategy lookupStrategy,
 	                                        AclCache aclCache ) {
 		super( dataSource, lookupStrategy, aclCache );
+	}
+
+	@PostConstruct
+	public void setupCache() {
+		if( cacheManager != null ) {
+			cache = cacheManager.getCache( AclSecurityConfiguration.CACHE_NAME );
+		}
+	}
+
+	@Override
+	protected Long createOrRetrieveSidPrimaryKey( Sid sid, boolean allowCreate ) {
+		Long sidId;
+		if( cache != null ) {
+			Long cachedSid = cache.get( sid, Long.class );
+			if( cachedSid != null ) {
+				return cachedSid;
+			} else {
+				sidId = super.createOrRetrieveSidPrimaryKey( sid, allowCreate );
+				cache.put( sid, sidId );
+			}
+		} else {
+			sidId = super.createOrRetrieveSidPrimaryKey( sid, allowCreate );
+		}
+		return sidId;
+	}
+
+	@Override
+	protected Long createOrRetrieveClassPrimaryKey( String type, boolean allowCreate ) {
+		Long classId;
+		if( cache != null ) {
+			Long cachedClassId = cache.get( type, Long.class );
+			if( cachedClassId != null ) {
+				return cachedClassId;
+			} else {
+				classId = super.createOrRetrieveClassPrimaryKey( type, allowCreate );
+				cache.put( type, classId );
+			}
+		} else {
+			classId = super.createOrRetrieveClassPrimaryKey( type, allowCreate );
+		}
+		return classId;
 	}
 }
