5f1bd9173995d9f410026e61dcd60e8bf16aa2c6
Arne Vandamme <arne@foreach.be>
2014-11-01 15:41:02 +0100

    Starting group management pages

---
+++ admin-web-module/src/main/java/com/foreach/across/modules/adminweb/menu/EntityAdminMenuEvent.java
@@ -0,0 +1,24 @@
+package com.foreach.across.modules.adminweb.menu;
+
+import com.foreach.across.modules.web.events.BuildMenuEvent;
+import com.foreach.across.modules.web.menu.PathBasedMenuBuilder;
+import org.springframework.core.ResolvableType;
+
+public class EntityAdminMenuEvent<T> extends BuildMenuEvent<EntityAdminMenu<T>>
+{
+	public EntityAdminMenuEvent( EntityAdminMenu<T> menu, PathBasedMenuBuilder menuBuilder ) {
+		super( menu, menuBuilder, ResolvableType.forClass( menu.getEntityClass() ) );
+	}
+
+	public Class<T> getEntityClass() {
+		return getMenu().getEntityClass();
+	}
+
+	public T getEntity() {
+		return getMenu().getEntity();
+	}
+
+	public boolean isExisting() {
+		return getEntity() != null;
+	}
+}
