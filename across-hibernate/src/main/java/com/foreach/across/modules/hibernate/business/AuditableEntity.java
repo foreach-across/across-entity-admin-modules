e1980b9d7adf956189fe0da566b0f53a76415b06
Andy Somers <asm@foreach.be>
2014-11-28 10:43:22 +0100

    added Auditable interface and default String implementation

---
+++ across-hibernate/src/main/java/com/foreach/across/modules/hibernate/business/AuditableEntity.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2014 the original author or authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.foreach.across.modules.hibernate.business;
+
+import javax.persistence.Column;
+import java.util.Date;
+
+/**
+ * @author Andy Somers
+ */
+public abstract class AuditableEntity implements Auditable<String>
+{
+	@Column(name = "created_by", nullable = true)
+	private String createdBy;
+	@Column(name = "created_date", nullable = true)
+	private Date createdDate;
+	@Column(name = "last_modified_by", nullable = true)
+	private String lastModifiedBy;
+	@Column(name = "last_modified_date", nullable = true)
+	private Date lastModifiedDate;
+
+	@Override
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy( String createdBy ) {
+		this.createdBy = createdBy;
+	}
+
+	@Override
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate( Date createdDate ) {
+		this.createdDate = createdDate;
+	}
+
+	@Override
+	public String getLastModifiedBy() {
+		return lastModifiedBy;
+	}
+
+	public void setLastModifiedBy( String lastModifiedBy ) {
+		this.lastModifiedBy = lastModifiedBy;
+	}
+
+	@Override
+	public Date getLastModifiedDate() {
+		return lastModifiedDate;
+	}
+
+	public void setLastModifiedDate( Date lastModifiedDate ) {
+		this.lastModifiedDate = lastModifiedDate;
+	}
+}
