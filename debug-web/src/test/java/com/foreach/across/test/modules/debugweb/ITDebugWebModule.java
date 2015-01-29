f78530151cd4c471c1cfbdd8169bf919a862a313
Arne Vandamme <arne@foreach.be>
2015-01-29 16:57:07 +0100

    Refactor Role entity - add validation annotations to usermodule entities

---
+++ debug-web/src/test/java/com/foreach/across/test/modules/debugweb/ITDebugWebModule.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2014 the original author or authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.foreach.across.test.modules.debugweb;
+
+import com.foreach.across.config.AcrossContextConfigurer;
+import com.foreach.across.core.AcrossContext;
+import com.foreach.across.modules.debugweb.DebugWeb;
+import com.foreach.across.modules.debugweb.DebugWebModule;
+import com.foreach.across.modules.web.mvc.PrefixingRequestMappingHandlerMapping;
+import com.foreach.across.modules.web.resource.WebResourcePackageManager;
+import com.foreach.across.modules.web.resource.WebResourceRegistryInterceptor;
+import com.foreach.across.modules.web.template.WebTemplateRegistry;
+import com.foreach.across.test.AcrossTestWebConfiguration;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@DirtiesContext
+@WebAppConfiguration
+@ContextConfiguration(classes = ITDebugWebModule.Config.class)
+public class ITDebugWebModule
+{
+	@Autowired
+	@Qualifier("debugWebTemplateRegistry")
+	private WebTemplateRegistry debugWebTemplateRegistry;
+
+	@Autowired
+	@Qualifier("debugWebResourcePackageManager")
+	private WebResourcePackageManager debugWebResourcePackageManager;
+
+	@Autowired
+	@Qualifier("debugWebResourceRegistryInterceptor")
+	private WebResourceRegistryInterceptor debugWebResourceRegistryInterceptor;
+
+	@Autowired
+	@Qualifier("debugHandlerMapping")
+	private PrefixingRequestMappingHandlerMapping debugHandlerMapping;
+
+	@Autowired
+	private DebugWeb debugWeb;
+
+	@Test
+	public void bootstrapModule() {
+		assertNotNull( debugWeb );
+		assertEquals( "/development/debug", debugWeb.getPathPrefix() );
+
+		assertNotNull( debugWebTemplateRegistry );
+		assertNotNull( debugWebResourcePackageManager );
+		assertNotNull( debugWebResourceRegistryInterceptor );
+		assertNotNull( debugHandlerMapping );
+	}
+
+	@Configuration
+	@AcrossTestWebConfiguration
+	protected static class Config implements AcrossContextConfigurer
+	{
+		@Override
+		public void configure( AcrossContext context ) {
+			context.addModule( debugWebModule() );
+		}
+
+		private DebugWebModule debugWebModule() {
+			DebugWebModule module = new DebugWebModule();
+			module.setRootPath( "/development/debug" );
+			return module;
+		}
+	}
+}
---
+++ oauth2-module/src/main/java/com/foreach/across/modules/oauth2/services/OAuth2ServiceImpl.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2014 the original author or authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.foreach.across.modules.oauth2.services;
+
+import com.foreach.across.modules.oauth2.business.OAuth2Client;
+import com.foreach.across.modules.oauth2.business.OAuth2Scope;
+import com.foreach.across.modules.oauth2.repositories.OAuth2ClientRepository;
+import com.foreach.across.modules.oauth2.repositories.OAuth2ScopeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+
+@Service
+public class OAuth2ServiceImpl implements OAuth2Service
+{
+	@Autowired
+	private OAuth2ScopeRepository oAuth2ScopeRepository;
+
+	@Autowired
+	private OAuth2ClientRepository oAuth2ClientRepository;
+
+	@Override
+	public Collection<OAuth2Client> getOAuth2Clients() {
+		return oAuth2ClientRepository.findAll();
+	}
+
+	@Override
+	public Collection<OAuth2Scope> getOAuth2Scopes() {
+		return oAuth2ScopeRepository.findAll();
+	}
+
+	@Override
+	public OAuth2Scope saveScope( OAuth2Scope oAuth2Scope ) {
+		return oAuth2ScopeRepository.save( oAuth2Scope );
+	}
+
+	@Override
+	public OAuth2Scope getScopeById( long id ) {
+		return oAuth2ScopeRepository.findOne( id );
+	}
+
+	@Override
+	public OAuth2Client saveClient( OAuth2Client oAuth2Client ) {
+		return oAuth2ClientRepository.save( oAuth2Client );
+	}
+
+	@Override
+	public OAuth2Client getClientById( String clientId ) {
+		return oAuth2ClientRepository.findByClientId( clientId );
+	}
+}
