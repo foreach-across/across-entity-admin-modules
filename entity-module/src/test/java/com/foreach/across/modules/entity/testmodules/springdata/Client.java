2494ce5a15a960b0616510d86a575207b61a083b
Arne Vandamme <arne@foreach.be>
2015-01-29 11:09:31 +0100

    Default equals semantics for SettableIdBasedEntity

--- entity-module/src/test/java/com/foreach/across/modules/entity/testmodules/springdata/Client.java
+++ entity-module/src/test/java/com/foreach/across/modules/entity/testmodules/springdata/Client.java
@@ -76,32 +76,4 @@ public class Client extends SettableIdBasedEntity<Client>
 
 		return client;
 	}
-
-	@Override
-	public boolean equals( Object o ) {
-		if ( this == o ) {
-			return true;
-		}
-		if ( o == null || getClass() != o.getClass() ) {
-			return false;
-		}
-
-		Client client = (Client) o;
-
-		if ( id != null ? !id.equals( client.id ) : client.id != null ) {
-			return false;
-		}
-		if ( name != null ? !name.equals( client.name ) : client.name != null ) {
-			return false;
-		}
-
-		return true;
-	}
-
-	@Override
-	public int hashCode() {
-		int result = id != null ? id.hashCode() : 0;
-		result = 31 * result + ( name != null ? name.hashCode() : 0 );
-		return result;
-	}
 }
--- entity-module/src/test/java/com/foreach/across/modules/entity/testmodules/springdata2/Client.java
+++ entity-module/src/test/java/com/foreach/across/modules/entity/testmodules/springdata2/Client.java
@@ -70,27 +70,4 @@ public class Client extends SettableIdBasedEntity<Client>
 
 		return client;
 	}
-
-	@Override
-	public boolean equals( Object o ) {
-		if ( this == o ) {
-			return true;
-		}
-		if ( o == null || getClass() != o.getClass() ) {
-			return false;
-		}
-
-		Client client = (Client) o;
-
-		if ( name != null ? !name.equals( client.name ) : client.name != null ) {
-			return false;
-		}
-
-		return true;
-	}
-
-	@Override
-	public int hashCode() {
-		return name != null ? name.hashCode() : 0;
-	}
 }
