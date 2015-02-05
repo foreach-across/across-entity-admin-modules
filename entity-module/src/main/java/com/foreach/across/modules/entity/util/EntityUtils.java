af3c73a3c937a1bcbed4391d3edb034470d7f569
Niels Doucet <niels.doucet@mediafin.be>
2015-02-05 08:15:39 +0100

    WIP PropertyPersistenceMetadata en embedded properties rendering

---
+++ entity-module/src/main/java/com/foreach/across/modules/entity/util/EntityUtils.java
@@ -0,0 +1,35 @@
+package com.foreach.across.modules.entity.util;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.beans.PropertyDescriptor;
+import java.util.LinkedList;
+import java.util.List;
+
+public class EntityUtils
+{
+	public static String generateDisplayName( String propertyName ) {
+		String cleaned = propertyName.replace( '.', ' ' ).replace( '_', ' ' ).replaceAll( "[^\\p{L}\\p{Nd} ]+", "" );
+
+		List<String> finished = new LinkedList<>();
+		for ( String part : StringUtils.split( cleaned, ' ' ) ) {
+			String capitalized = StringUtils.isAllUpperCase( part )
+					? StringUtils.capitalize( StringUtils.lowerCase( part ) ) : StringUtils.capitalize( part );
+
+			for ( String subPart : StringUtils.splitByCharacterTypeCamelCase( capitalized ) ) {
+				finished.add( finished.isEmpty() ? subPart : StringUtils.lowerCase( subPart ) );
+			}
+		}
+
+		return StringUtils.join( finished, " " );
+	}
+
+	public static Object getPropertyValue( PropertyDescriptor descriptor, Object instance ) {
+		try {
+			return descriptor.getReadMethod().invoke( instance );
+		}
+		catch ( Exception e ) {
+			return null;
+		}
+	}
+}
