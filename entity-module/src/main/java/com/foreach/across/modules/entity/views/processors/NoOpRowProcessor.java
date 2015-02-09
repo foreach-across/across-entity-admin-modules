d09bc6edc3afdac6b578dd90817c104f0b53bc35
Niels Doucet <niels.doucet@mediafin.be>
2015-02-09 07:59:33 +0100

    - move thymeleaf dialect configuration to webconfiguration - reformat according to project codestylesettings

---
+++ entity-module/src/main/java/com/foreach/across/modules/entity/views/processors/NoOpRowProcessor.java
@@ -0,0 +1,13 @@
+package com.foreach.across.modules.entity.views.processors;
+
+/**
+ * @author niels
+ * @since 6/02/2015
+ */
+public class NoOpRowProcessor<T> implements RowProcessor<T>
+{
+	@Override
+	public String process( T entity ) {
+		return null;
+	}
+}
