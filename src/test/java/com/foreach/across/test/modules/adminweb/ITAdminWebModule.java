3604aefb47ae0f339d99fb2c1b9b8e593350dfa4
Arne Vandamme <arne@foreach.be>
2015-02-11 15:10:57 +0100

    Merge in develop - fix tests

--- src/test/java/com/foreach/across/test/modules/adminweb/ITAdminWebModule.java
+++ src/test/java/com/foreach/across/test/modules/adminweb/ITAdminWebModule.java
@@ -58,7 +58,7 @@ public class ITAdminWebModule
 	private WebResourceRegistryInterceptor adminWebResourceRegistryInterceptor;
 
 	@Autowired
-	@Qualifier("adminRequestMappingHandlerMapping")
+	@Qualifier("adminWebHandlerMapping")
 	private PrefixingRequestMappingHandlerMapping adminRequestMappingHandlerMapping;
 
 	@Autowired
