2c9a6c632a42327e0b94c7f1008e28288b9f2523
Arne Vandamme <arne@foreach.be>
2015-02-11 11:44:31 +0100

    Use new PrefixingHandlerMappingConfiguration for AdminWeb and DebugWeb - optimize interceptor registration

--- admin-web-module/src/main/java/com/foreach/across/modules/adminweb/config/AdminWebMvcConfiguration.java
+++ admin-web-module/src/main/java/com/foreach/across/modules/adminweb/config/AdminWebMvcConfiguration.java
@@ -18,10 +18,6 @@ package com.foreach.across.modules.adminweb.config;
 import com.foreach.across.core.AcrossModule;
 import com.foreach.across.core.annotations.Exposed;
 import com.foreach.across.core.annotations.Module;
-import com.foreach.across.core.context.info.AcrossContextInfo;
-import com.foreach.across.core.context.registry.AcrossContextBeanRegistry;
-import com.foreach.across.core.development.AcrossDevelopmentMode;
-import com.foreach.across.modules.adminweb.AdminWeb;
 import com.foreach.across.modules.adminweb.AdminWebModule;
 import com.foreach.across.modules.adminweb.annotations.AdminWebController;
 import com.foreach.across.modules.adminweb.controllers.AuthenticationController;
@@ -29,46 +25,23 @@ import com.foreach.across.modules.adminweb.menu.AdminMenu;
 import com.foreach.across.modules.adminweb.menu.AdminMenuBuilder;
 import com.foreach.across.modules.adminweb.menu.EntityAdminMenu;
 import com.foreach.across.modules.adminweb.menu.EntityAdminMenuBuilder;
-import com.foreach.across.modules.adminweb.resource.BootstrapWebResourcePackage;
-import com.foreach.across.modules.adminweb.resource.JQueryWebResourcePackage;
+import com.foreach.across.modules.web.config.support.PrefixingHandlerMappingConfiguration;
 import com.foreach.across.modules.web.menu.MenuFactory;
 import com.foreach.across.modules.web.mvc.PrefixingRequestMappingHandlerMapping;
-import com.foreach.across.modules.web.resource.*;
-import com.foreach.across.modules.web.template.LayoutTemplateProcessorAdapterBean;
-import com.foreach.across.modules.web.template.WebTemplateInterceptor;
-import com.foreach.across.modules.web.template.WebTemplateRegistry;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.aop.ClassFilter;
 import org.springframework.aop.support.annotation.AnnotationClassFilter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.orm.hibernate4.support.OpenSessionInViewInterceptor;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import javax.annotation.PostConstruct;
 
 @Configuration
-public class AdminWebMvcConfiguration extends WebMvcConfigurerAdapter
+public class AdminWebMvcConfiguration extends PrefixingHandlerMappingConfiguration
 {
-	private static final Logger LOG = LoggerFactory.getLogger( AdminWebModule.class );
-
-	@Autowired(required = false)
-	private WebResourceTranslator viewsWebResourceTranslator;
-
-	@Autowired
-	private AcrossContextInfo contextInfo;
-
-	@Autowired
-	private AcrossContextBeanRegistry beanRegistry;
-
 	@Autowired
 	@Module(AcrossModule.CURRENT_MODULE)
 	private AdminWebModule adminWebModule;
-
-	@Autowired
-	private AcrossDevelopmentMode developmentMode;
 
 	@Autowired
 	private MenuFactory menuFactory;
@@ -80,107 +53,32 @@ public class AdminWebMvcConfiguration extends WebMvcConfigurerAdapter
 		menuFactory.addMenuBuilder( entityAdminMenuBuilder(), EntityAdminMenu.class );
 	}
 
+	@Override
+	protected String getPrefixPath() {
+		return adminWebModule.getRootPath();
+	}
+
+	@Override
+	protected ClassFilter getHandlerMatcher() {
+		return new AnnotationClassFilter( AdminWebController.class, true );
+	}
+
+	@Bean(name = "adminWebHandlerMapping")
+	@Exposed
+	@Override
+	public PrefixingRequestMappingHandlerMapping controllerHandlerMapping() {
+		return super.controllerHandlerMapping();
+	}
+
 	@Bean
 	public AdminMenuBuilder adminMenuBuilder() {
 		return new AdminMenuBuilder();
 	}
 
+	// todo: get out to entity module you!
 	@Bean
 	public EntityAdminMenuBuilder entityAdminMenuBuilder() {
 		return new EntityAdminMenuBuilder();
-	}
-
-	@Bean
-	@Exposed
-	public AdminWeb adminWeb() {
-		return new AdminWeb( adminWebModule.getRootPath(), "Administrative web interface" );
-	}
-
-	@Bean
-	@Exposed
-	public WebTemplateRegistry adminWebTemplateRegistry() {
-		WebTemplateRegistry webTemplateRegistry = new WebTemplateRegistry();
-
-		webTemplateRegistry.register( AdminWeb.LAYOUT_TEMPLATE, adminLayoutTemplateProcessor() );
-		webTemplateRegistry.setDefaultTemplateName( AdminWeb.LAYOUT_TEMPLATE );
-
-		return webTemplateRegistry;
-	}
-
-	// todo: verify thymeleaf support is enabled
-	@Bean
-	public LayoutTemplateProcessorAdapterBean adminLayoutTemplateProcessor() {
-		return new LayoutTemplateProcessorAdapterBean( AdminWeb.LAYOUT_TEMPLATE )
-		{
-			@Override
-			protected void registerWebResources( WebResourceRegistry registry ) {
-				registry.addPackage( BootstrapWebResourcePackage.NAME );
-				registry.addWithKey( WebResource.CSS, AdminWeb.MODULE, AdminWeb.LAYOUT_TEMPLATE_CSS,
-				                     WebResource.VIEWS );
-			}
-
-			@Override
-			protected void buildMenus( MenuFactory menuFactory ) {
-				menuFactory.buildMenu( AdminMenu.NAME, AdminMenu.class );
-			}
-		};
-	}
-
-	@Bean
-	public WebTemplateInterceptor adminWebTemplateInterceptor() {
-		return new WebTemplateInterceptor( adminWebTemplateRegistry() );
-	}
-
-	@Bean
-	@Exposed
-	public WebResourcePackageManager adminWebResourcePackageManager() {
-		WebResourcePackageManager webResourcePackageManager = new WebResourcePackageManager();
-		webResourcePackageManager.register( JQueryWebResourcePackage.NAME,
-		                                    new JQueryWebResourcePackage(
-				                                    !developmentMode.isActive() ) );
-		webResourcePackageManager.register( BootstrapWebResourcePackage.NAME,
-		                                    new BootstrapWebResourcePackage(
-				                                    !developmentMode.isActive() ) );
-
-		return webResourcePackageManager;
-	}
-
-	@Bean
-	@Exposed
-	public WebResourceRegistryInterceptor adminWebResourceRegistryInterceptor() {
-		WebResourceRegistryInterceptor interceptor =
-				new WebResourceRegistryInterceptor( adminWebResourcePackageManager() );
-
-		if ( viewsWebResourceTranslator != null ) {
-			interceptor.addWebResourceTranslator( viewsWebResourceTranslator );
-		}
-		else {
-			LOG.warn( "No default viewsWebResourceTranslator configured - manual translators will be required." );
-		}
-
-		return interceptor;
-	}
-
-	@Bean
-	@Exposed
-	public PrefixingRequestMappingHandlerMapping adminRequestMappingHandlerMapping() {
-		PrefixingRequestMappingHandlerMapping mappingHandlerMapping =
-				new PrefixingRequestMappingHandlerMapping( adminWebModule.getRootPath(),
-				                                           new AnnotationClassFilter( AdminWebController.class,
-				                                                                      true ) );
-		// todo: unify web registration approach and move this to a different configuration
-		if ( contextInfo.hasModule( "AcrossHibernateModule" ) ) {
-			ApplicationContext moduleCtx = contextInfo.getModuleInfo( "AcrossHibernateModule" ).getApplicationContext();
-
-			if ( moduleCtx.containsLocalBean( "openSessionInViewInterceptor" ) ) {
-				mappingHandlerMapping.addInterceptor( moduleCtx.getBean( "openSessionInViewInterceptor",
-				                                                         OpenSessionInViewInterceptor.class ) );
-			}
-		}
-
-		mappingHandlerMapping.addInterceptor( adminWebResourceRegistryInterceptor(), adminWebTemplateInterceptor() );
-
-		return mappingHandlerMapping;
 	}
 
 	@Bean
