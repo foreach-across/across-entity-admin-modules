0a0e6c0f7179bfc07b859ac4379419039c31e332
Arne Vandamme <arne@foreach.be>
2015-03-05 11:24:50 +0100

    AXSTD-47 Filter entities on association lists

--- entity-module/src/test/java/com/foreach/across/modules/entity/testmodules/springdata/business/ClientGroup.java
+++ entity-module/src/test/java/com/foreach/across/modules/entity/testmodules/springdata/business/ClientGroup.java
@@ -56,4 +56,27 @@ public class ClientGroup implements Persistable<ClientGroupId>
 	public boolean isNew() {
 		return id == null || id.getClient() == null || id.getGroup() == null;
 	}
+
+	@Override
+	public boolean equals( Object o ) {
+		if ( this == o ) {
+			return true;
+		}
+		if ( o == null || getClass() != o.getClass() ) {
+			return false;
+		}
+
+		ClientGroup that = (ClientGroup) o;
+
+		if ( id != null ? !id.equals( that.id ) : that.id != null ) {
+			return false;
+		}
+
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		return id != null ? id.hashCode() : 0;
+	}
 }
