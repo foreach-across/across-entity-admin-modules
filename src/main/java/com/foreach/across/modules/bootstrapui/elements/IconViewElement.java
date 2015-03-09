eebf578cb7d7a86839cd8ef518ab8bbd6d7427c2
Arne Vandamme <arne@foreach.be>
2015-03-09 14:54:19 +0100

    IconViewElement implementation

---
+++ src/main/java/com/foreach/across/modules/bootstrapui/elements/IconViewElement.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2014 the original author or authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.foreach.across.modules.bootstrapui.elements;
+
+import com.foreach.across.modules.web.ui.elements.SingleNodeViewElementSupport;
+import org.springframework.util.Assert;
+
+/**
+ * Represents a Bootstrap Glyphicon.
+ *
+ * @author Arne Vandamme
+ */
+public class IconViewElement extends SingleNodeViewElementSupport
+{
+	public static final String TYPE = BootstrapUiElements.ICON;
+
+	private String glyph;
+
+	public IconViewElement( String glyph ) {
+		this();
+		this.glyph = glyph;
+	}
+
+	public IconViewElement() {
+		super( TYPE, "span" );
+
+		setAttribute( "aria-hidden", "true" );
+	}
+
+	@Override
+	public String getTagName() {
+		return super.getTagName();
+	}
+
+	@Override
+	public void setTagName( String tagName ) {
+		super.setTagName( tagName );
+	}
+
+	public String getGlyph() {
+		return glyph;
+	}
+
+	public void setGlyph( String glyph ) {
+		Assert.notNull( glyph );
+		this.glyph = glyph;
+	}
+}
