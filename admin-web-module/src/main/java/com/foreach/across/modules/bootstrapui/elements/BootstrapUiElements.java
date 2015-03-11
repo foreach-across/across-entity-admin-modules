06ddcb68453db2f082cef4998039a1ab3074cc87
Arne Vandamme <arne@foreach.be>
2015-03-11 13:01:24 +0100

    LabelFormElement - WIP

--- admin-web-module/src/main/java/com/foreach/across/modules/bootstrapui/elements/BootstrapUiElements.java
+++ admin-web-module/src/main/java/com/foreach/across/modules/bootstrapui/elements/BootstrapUiElements.java
@@ -31,4 +31,5 @@ public interface BootstrapUiElements extends StandardViewElements
 	public static final String RADIO = "bootstrapRadio";
 	public static final String SELECT = "bootstrapSelect";
 	public static final String STATIC_CONTROL = "bootstrapStaticControl";
+	public static final String LABEL = "bootstrapLabel";
 }
