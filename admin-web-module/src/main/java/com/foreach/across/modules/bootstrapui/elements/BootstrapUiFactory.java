f05f7aa5f172b533a488ce8ae42ba390581098b2
Arne Vandamme <arne@foreach.be>
2015-03-29 20:58:06 +0200

    Button, FormGroup, Label and Textbox element builders

--- admin-web-module/src/main/java/com/foreach/across/modules/bootstrapui/elements/BootstrapUiFactory.java
+++ admin-web-module/src/main/java/com/foreach/across/modules/bootstrapui/elements/BootstrapUiFactory.java
@@ -1,9 +1,26 @@
 package com.foreach.across.modules.bootstrapui.elements;
 
-import com.foreach.across.modules.bootstrapui.elements.builder.FormViewElementBuilder;
+import com.foreach.across.modules.bootstrapui.elements.builder.*;
+import com.foreach.across.modules.web.ui.ViewElementBuilder;
 import com.foreach.across.modules.web.ui.ViewElementBuilderFactory;
 
 public interface BootstrapUiFactory extends ViewElementBuilderFactory
 {
 	FormViewElementBuilder form();
+
+	LabelFormElementBuilder label();
+
+	LabelFormElementBuilder label( String labelText );
+
+	FormGroupElementBuilder formGroup();
+
+	FormGroupElementBuilder formGroup( ViewElementBuilder label, ViewElementBuilder control );
+
+	ButtonViewElementBuilder button();
+
+	TextboxFormElementBuilder textbox();
+
+	TextboxFormElementBuilder textarea();
+
+	TextboxFormElementBuilder password();
 }
