1cdd8c5d4b75cc0b8b64a4df7eb6c1ae1e02b673
Andy Somers <asm@foreach.be>
2015-06-22 15:25:44 +0200

    implemented iterable interception

--- across-hibernate/src/test/java/com/foreach/across/modules/hibernate/testmodules/springdata/ClientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.foreach.across.modules.hibernate.testmodules.springdata;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ClientRepository extends CrudRepository<Client, Long>
-{
-}
