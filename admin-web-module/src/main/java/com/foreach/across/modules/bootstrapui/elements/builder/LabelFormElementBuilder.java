9109c92aac854214f921cc2f32e48a48e4bbfb9c
Arne Vandamme <arne@foreach.be>
2015-08-05 17:30:59 +0200

    DateTimeFormElement wip - fix issue where label text was not used as placeholder - label for input group points to the first form control element

--- admin-web-module/src/main/java/com/foreach/across/modules/bootstrapui/elements/builder/LabelFormElementBuilder.java
+++ admin-web-module/src/main/java/com/foreach/across/modules/bootstrapui/elements/builder/LabelFormElementBuilder.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2014 the original author or authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.foreach.across.modules.bootstrapui.elements.builder;
 
 import com.foreach.across.modules.bootstrapui.elements.LabelFormElement;
@@ -5,13 +20,14 @@ import com.foreach.across.modules.web.ui.ViewElement;
 import com.foreach.across.modules.web.ui.ViewElementBuilder;
 import com.foreach.across.modules.web.ui.ViewElementBuilderContext;
 import com.foreach.across.modules.web.ui.ViewElementPostProcessor;
+import com.foreach.across.modules.web.ui.elements.TextViewElement;
 import com.foreach.across.modules.web.ui.elements.builder.NodeViewElementSupportBuilder;
 
 import java.util.Map;
 
 public class LabelFormElementBuilder extends NodeViewElementSupportBuilder<LabelFormElement, LabelFormElementBuilder>
 {
-	private String text;
+	private Object text;
 	private Object target;
 
 	@Override
@@ -79,6 +95,19 @@ public class LabelFormElementBuilder extends NodeViewElementSupportBuilder<Label
 		return this;
 	}
 
+	/**
+	 * Configure a builder that should supply the text to be rendered.  If the builder produces a single
+	 * {@link TextViewElement} with plain text, it will not be added as a child of the label, but used as
+	 * the text property.  In all other cases the element will be the first child of the label.
+	 *
+	 * @param textBuilder responsible for generating the text
+	 * @return current builder
+	 */
+	public LabelFormElementBuilder text( ViewElementBuilder textBuilder ) {
+		text = textBuilder;
+		return this;
+	}
+
 	public LabelFormElementBuilder target( String htmlId ) {
 		this.target = htmlId;
 		return this;
@@ -98,8 +127,29 @@ public class LabelFormElementBuilder extends NodeViewElementSupportBuilder<Label
 	protected LabelFormElement createElement( ViewElementBuilderContext viewElementBuilderContext ) {
 		LabelFormElement label = new LabelFormElement();
 
-		if ( text != null ) {
-			label.setText( text );
+		if ( text instanceof String ) {
+			label.setText( (String) text );
+		}
+		else if ( text instanceof ViewElementBuilder ) {
+			ViewElementBuilder textBuilder = (ViewElementBuilder) text;
+			ViewElement textElement = textBuilder.build( viewElementBuilderContext );
+
+			if ( textElement != null ) {
+				boolean addAsChild = true;
+
+				if ( textElement instanceof TextViewElement ) {
+					TextViewElement textViewElement = (TextViewElement) textElement;
+
+					if ( textViewElement.isEscapeXml() && textViewElement.getCustomTemplate() ==null ) {
+						label.setText( textViewElement.getText() );
+						addAsChild = false;
+					}
+				}
+
+				if ( addAsChild ) {
+					label.addFirst( textElement );
+				}
+			}
 		}
 
 		if ( target != null ) {
@@ -113,6 +163,7 @@ public class LabelFormElementBuilder extends NodeViewElementSupportBuilder<Label
 				label.setTarget( ( (ViewElementBuilder) target ).build( viewElementBuilderContext ) );
 			}
 		}
+
 		return apply( label, viewElementBuilderContext );
 	}
 }
--- admin-web-module/src/main/java/com/foreach/across/modules/bootstrapui/utils/BootstrapElementUtils.java
+++ admin-web-module/src/main/java/com/foreach/across/modules/bootstrapui/utils/BootstrapElementUtils.java
@@ -15,7 +15,7 @@
  */
 package com.foreach.across.modules.bootstrapui.utils;
 
-import com.foreach.across.modules.bootstrapui.elements.FormControlElementSupport;
+import com.foreach.across.modules.bootstrapui.elements.FormControlElement;
 import com.foreach.across.modules.bootstrapui.elements.FormGroupElement;
 import com.foreach.across.modules.bootstrapui.elements.InputGroupFormElement;
 import com.foreach.across.modules.web.ui.ViewElement;
@@ -30,22 +30,22 @@ public final class BootstrapElementUtils
 	}
 
 	/**
-	 * Will get the first {@link FormControlElementSupport} instance.  If the {@link ViewElement} is
-	 * a {@link FormControlElementSupport} it will be returned.  If it is a
+	 * Will get the first {@link FormControlElement} instance.  If the {@link ViewElement} is
+	 * a {@link FormControlElement} it will be returned.  If it is a
 	 * {@link com.foreach.across.modules.bootstrapui.elements.InputGroupFormElement} the input group control will
 	 * be returned instead if it matches.  If the {@link ViewElement} is a
 	 * {@link com.foreach.across.modules.web.ui.ViewElementCollection} then a recursive search will happen
-	 * until the first element extending {@link FormControlElementSupport} is returned.
+	 * until the first element extending {@link FormControlElement} is returned.
 	 *
 	 * @param viewElement instance
 	 * @return form control instance, or {@code null} if not found
 	 */
-	public static FormControlElementSupport getFormControl( ViewElement viewElement ) {
-		return getFormControl( viewElement, FormControlElementSupport.class );
+	public static FormControlElement getFormControl( ViewElement viewElement ) {
+		return getFormControl( viewElement, FormControlElement.class );
 	}
 
 	/**
-	 * Will get the first {@link FormControlElementSupport} instance matching the expected type.
+	 * Will get the first {@link FormControlElement} instance matching the expected type.
 	 * If the {@link ViewElement} is of the expected type it will be returned.  If it isIf it is a
 	 * {@link com.foreach.across.modules.bootstrapui.elements.InputGroupFormElement} the input group control will
 	 * be returned instead if it matches.  If the {@link ViewElement} is a
@@ -56,8 +56,8 @@ public final class BootstrapElementUtils
 	 * @param expectedType the control should have
 	 * @return form control instance, or {@code null} if not found
 	 */
-	public static <V extends FormControlElementSupport> V getFormControl( ViewElement viewElement,
-	                                                                      Class<V> expectedType ) {
+	public static <V extends FormControlElement> V getFormControl( ViewElement viewElement,
+	                                                               Class<V> expectedType ) {
 		ViewElement target = viewElement;
 
 		if ( expectedType.isInstance( viewElement ) ) {
@@ -67,7 +67,7 @@ public final class BootstrapElementUtils
 		V control = null;
 
 		if ( target instanceof FormGroupElement ) {
-			ViewElement candidate =  ( (FormGroupElement) target ).getControl();
+			ViewElement candidate = ( (FormGroupElement) target ).getControl();
 			if ( candidate != null ) {
 				if ( expectedType.isInstance( candidate ) ) {
 					control = (V) candidate;
