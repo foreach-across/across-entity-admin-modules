93565f2794846038cb7bbb9b268813348f44c422
Steven Gentens <stg@foreach.be>
2018-08-09 16:19:41 +0200

    AXAWM-18 upgrade to 3.0.2-SNAPSHOT - added UserContextAdminMenuItem event to register an icon / displayname

--- admin-web-module/src/main/java/com/foreach/across/modules/adminweb/menu/registrars/DefaultAdminMenuRegistrar.java
+++ admin-web-module/src/main/java/com/foreach/across/modules/adminweb/menu/registrars/DefaultAdminMenuRegistrar.java
@@ -16,6 +16,8 @@
 
 package com.foreach.across.modules.adminweb.menu.registrars;
 
+import com.foreach.across.core.annotations.OrderInModule;
+import com.foreach.across.modules.adminweb.events.UserContextAdminMenuItem;
 import com.foreach.across.modules.adminweb.menu.AdminMenu;
 import com.foreach.across.modules.adminweb.menu.AdminMenuEvent;
 import com.foreach.across.modules.adminweb.ui.AdminWebLayoutTemplate;
@@ -23,11 +25,16 @@ import com.foreach.across.modules.bootstrapui.components.builder.NavComponentBui
 import com.foreach.across.modules.bootstrapui.components.builder.PanelsNavComponentBuilder;
 import com.foreach.across.modules.bootstrapui.elements.GlyphIcon;
 import com.foreach.across.modules.spring.security.infrastructure.services.CurrentSecurityPrincipalProxy;
+import com.foreach.across.modules.web.menu.PathBasedMenuBuilder;
+import com.foreach.across.modules.web.ui.elements.builder.VoidNodeViewElementBuilder;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.event.EventListener;
 import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
+
+import static com.foreach.across.modules.adminweb.events.UserContextAdminMenuItem.USER_CONTEXT_MENU_ITEM;
 
 /**
  * Registers the user context menu item to render in the right navbar on the default template.
@@ -41,9 +48,11 @@ public final class DefaultAdminMenuRegistrar
 {
 	public static final String PATH = "/user-context";
 
+	private final ApplicationEventPublisher eventPublisher;
 	private final CurrentSecurityPrincipalProxy securityPrincipalProxy;
 
 	@EventListener
+	@OrderInModule(1000)
 	void registerDefaultItems( AdminMenuEvent menuEvent ) {
 		menuEvent.builder()
 		         .root( "/" )
@@ -60,4 +69,37 @@ public final class DefaultAdminMenuRegistrar
 		         .and()
 		         .item( PATH + "/logout", "Logout", "@adminWeb:/logout" );
 	}
+
+	/**
+	 * Registers a {@link UserContextAdminMenuItem} on the {@link AdminMenu}.
+	 * If a {@link UserContextAdminMenuItem#displayName} is registered, it will be displayed in the user context menu.
+	 * If a {@link UserContextAdminMenuItem#thumbnailUrl}
+	 * @param adminMenuEvent
+	 */
+	@EventListener
+	@OrderInModule(2000)
+	void registerUserContextAdminMenuItem( AdminMenuEvent adminMenuEvent ) {
+		UserContextAdminMenuItem userContextMenuItem = new UserContextAdminMenuItem();
+		userContextMenuItem.setDisplayName( securityPrincipalProxy.getPrincipalName() );
+		userContextMenuItem.setThumbnailUrl( "" );
+
+		eventPublisher.publishEvent( userContextMenuItem );
+
+		PathBasedMenuBuilder.PathBasedMenuItemBuilder group = adminMenuEvent.builder().group( PATH );
+		if ( StringUtils.isNotBlank( userContextMenuItem.getDisplayName() ) ) {
+			group.title( "  " + userContextMenuItem.getDisplayName() )
+			     .attribute( NavComponentBuilder.ATTR_ICON_ONLY, userContextMenuItem.getDisplayName().isEmpty() );
+		}
+
+		if ( StringUtils.isNotBlank( userContextMenuItem.getThumbnailUrl() ) ) {
+			group.attribute( NavComponentBuilder.ATTR_ICON_ONLY, false )
+			     .attribute( NavComponentBuilder.ATTR_ICON, new VoidNodeViewElementBuilder( "userContextThumbnail" )
+					     .tagName( "image" )
+					     .css( "user-context-thumbnail" )
+					     .attribute( "src", userContextMenuItem.getThumbnailUrl() )
+					     .build() );
+		}
+		adminMenuEvent.getMenu().setAttribute( USER_CONTEXT_MENU_ITEM, userContextMenuItem );
+	}
+
 }
