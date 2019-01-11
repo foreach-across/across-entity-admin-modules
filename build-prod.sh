170ba220023d89036eb54a03fe12fa838096fef1
svh <svh@foreach.be>
2019-01-11 15:10:17 +0100

    Working frontendSetup with docker

--- build-prod.sh
+++ build-prod.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-if ! docker-compose run --rm frontend sh -c "yarn run scss:prod && chmod 777 -R /build"; then
-    exit 1
-fi
-if ! docker-compose run --rm frontend sh -c "yarn run js:prod && chmod 777 -R /build"; then
-    exit 1
-fi
+docker-compose run --rm --user "bamboo" frontend sh -c "yarn run scss:prod && run js:prod"
