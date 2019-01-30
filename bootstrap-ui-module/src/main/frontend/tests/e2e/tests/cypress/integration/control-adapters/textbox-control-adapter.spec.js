58b96a2efc81fba8abf5841794aef44a28a8b3d5
Steven Gentens <stg@foreach.be>
2019-01-30 09:14:24 +0100

    AXBUM-30 refactor basic control adapter tests

--- bootstrap-ui-module/src/main/frontend/tests/e2e/tests/cypress/integration/control-adapters/textbox-control-adapter.spec.js
+++ bootstrap-ui-module/src/main/frontend/tests/e2e/tests/cypress/integration/control-adapters/textbox-control-adapter.spec.js
@@ -16,15 +16,12 @@
 
 import adapterUtils from "../../support/utils/control-adapters";
 
-describe( 'ControlAdapter - Textbox', function () {
+describe( 'ControlAdapter - Basic controls', function () {
     before( function () {
         cy.visit( "/control-adapters" );
     } );
 
-    describe( 'textbox', function () {
-        const selector = '#ca-textbox';
-        const content = 'now this is a story all about how';
-
+    let basicControlAdapterTests = function ( selector, content ) {
         afterEach( 'reset adapter', function () {
             cy.get( selector )
                     .then( ( element ) => {
@@ -70,108 +67,20 @@ describe( 'ControlAdapter - Textbox', function () {
                         adapterUtils.assertThatBootstrapUiChangeIsTriggeredOn( element, 'change' );
                     } )
         } );
+    };
+
+    describe( 'textbox', () => {
+        const content = 'now this is a story all about how';
+        basicControlAdapterTests( '#ca-textbox', content );
     } );
 
-    describe( 'autosizing textbox', function () {
-        const selector = '#ca-textbox-autosize';
+    describe( 'autosizing textbox', () => {
         const content = "my life got flipped turned upside down and I'd like to take a minute just sit right there";
-
-        afterEach( 'reset adapter', function () {
-            cy.get( selector )
-                    .then( ( element ) => {
-                        adapterUtils.getAdapterForElement( element ).reset();
-                    } )
-        } );
-        it( 'adapter exists', function () {
-            adapterUtils.assertThatAdapterExists( selector );
-        } );
-
-        it( 'does not have underlying control adapters', function () {
-            adapterUtils.assertHasUnderlyingControlAdapters( selector, 0 );
-        } );
-
-        it( 'value holds option label, option value and option item', function () {
-            cy.get( selector )
-                    .type( content )
-                    .then( ( element ) => {
-                        adapterUtils.assertAdapterValueSelected( element, 0, content, content, element[0] );
-                    } );
-        } );
-
-        it( 'modifying value', function () {
-            cy.get( selector )
-                    .then( ( element ) => adapterUtils.assertAdapterValueSelected( element, 0, '' ) )
-                    .type( content )
-                    .then( ( element ) => adapterUtils.assertAdapterValueSelected( element, 0, content ) );
-        } );
-
-        it( 'reset elements initial value', function () {
-            cy.get( selector )
-                    .type( content )
-                    .then( ( element ) => {
-                        adapterUtils.assertAdapterValueSelected( element, 0, content );
-                        adapterUtils.getAdapterForElement( element ).reset();
-                        adapterUtils.assertAdapterValueSelected( element, 0, '' );
-                    } )
-        } );
-
-        it( 'bootstrapui.change is fired when a change event is emitted on the element', function () {
-            cy.get( selector )
-                    .then( ( element ) => {
-                        adapterUtils.assertThatBootstrapUiChangeIsTriggeredOn( element, 'change' );
-                    } )
-        } );
+        basicControlAdapterTests( '#ca-textbox-autosize', content );
     } );
 
-    describe( 'textarea', function () {
-        const selector = '#ca-textarea';
+    describe( 'textarea', () => {
         const content = "I'll tell you how I become the prince of a town called Bel Air\n\n\n\n\n!!";
-
-        afterEach( 'reset adapter', function () {
-            cy.get( selector )
-                    .then( ( element ) => {
-                        adapterUtils.getAdapterForElement( element ).reset();
-                    } )
-        } );
-
-        it( 'adapter exists', function () {
-            adapterUtils.assertThatAdapterExists( selector );
-        } );
-
-        it( 'does not have underlying control adapters', function () {
-            adapterUtils.assertHasUnderlyingControlAdapters( selector, 0 );
-        } );
-
-        it( 'value holds option label, option value and option item', function () {
-            cy.get( selector )
-                    .type( content )
-                    .then( ( element ) => {
-                        adapterUtils.assertAdapterValueSelected( element, 0, content, content, element[0] );
-                    } );
-        } );
-
-        it( 'modifying value', function () {
-            cy.get( selector )
-                    .then( ( element ) => adapterUtils.assertAdapterValueSelected( element, 0, '' ) )
-                    .type( content )
-                    .then( ( element ) => adapterUtils.assertAdapterValueSelected( element, 0, content ) );
-        } );
-
-        it( 'reset elements initial value', function () {
-            cy.get( selector )
-                    .type( content )
-                    .then( ( element ) => {
-                        adapterUtils.assertAdapterValueSelected( element, 0, content );
-                        adapterUtils.getAdapterForElement( element ).reset();
-                        adapterUtils.assertAdapterValueSelected( element, 0, '' );
-                    } )
-        } );
-
-        it( 'bootstrapui.change is fired when a change event is emitted on the element', function () {
-            cy.get( selector )
-                    .then( ( element ) => {
-                        adapterUtils.assertThatBootstrapUiChangeIsTriggeredOn( element, 'change' );
-                    } )
-        } );
+        basicControlAdapterTests( '#ca-textarea', content );
     } );
 } );
