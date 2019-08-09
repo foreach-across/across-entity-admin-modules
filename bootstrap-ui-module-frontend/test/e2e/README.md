256b4dd81fcb9b303cd7035f3e34ba27b490845d
Simon VDB <simonvdbroeck@gmail.com>
2019-08-09 08:30:50 +0200

    copy over test folder

---
+++ bootstrap-ui-module-frontend/test/e2e/README.md
@@ -0,0 +1,23 @@
+# Installing cypress
+
+- navigate to `bootstrap-ui-module:src/main/frontend/e2e`
+- install the cypress dependency by using `yarn install`
+- Optional: install npx globally by using `npm install -g npx`
+
+# Starting Cypress
+
+Navigate to `bootstrap-ui-module/src/main/frontend/e2e/tests`
+
+## Open the Cypress Test Runner
+
+- Start the BootstrapUiTestApplication
+- `"../node_modules/.bin/cypress" open` (or `npx cypress open`) opens the GUI through which you can run all or specific tests
+
+## Running tests
+
+Press `Run all specs` in the Cypress GUI or by using npx `npx cypress run`.
+By adding the `--headed` option, an electron browser will be used instead of running headlessly.
+
+To run specific files, provide the spec option with a quoted path or comma separated file paths: `npx cypress run --spec './cypress/integration/**/*'`
+
+See the [cypress command line](https://docs.cypress.io/guides/guides/command-line.html#Installation) for more information.
---
+++ bootstrap-ui-module-frontend/test/e2e/tests/cypress/integration/control-adapters/basic-control-adapter.spec.js
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2019 the original author or authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import adapterUtils from "../../support/utils/control-adapters";
+
+describe( 'ControlAdapter - Basic', function () {
+    const basicAdapterTests = function ( selector, content ) {
+        afterEach( 'reset adapter', function () {
+            cy.get( selector )
+                    .then( ( element ) => {
+                        adapterUtils.getAdapterForElement( element ).reset();
+                    } )
+        } );
+
+        it( 'adapter exists', function () {
+            adapterUtils.assertThatAdapterExists( selector );
+        } );
+
+        it( 'does not have underlying control adapters', function () {
+            adapterUtils.assertHasUnderlyingControlAdapters( selector, 0 );
+        } );
+
+        it( "value holds the textbox and it's content as label and value", function () {
+            cy.get( selector )
+                    .type( content )
+                    .then( ( element ) => {
+                        adapterUtils.assertAdapterValueSelected( element, 0, content, content, element[0] );
+                    } );
+        } );
+
+        it( 'modifying value', function () {
+            cy.get( selector )
+                    .then( ( element ) => adapterUtils.assertAdapterValueSelected( element, 0, '' ) )
+                    .type( content )
+                    .then( ( element ) => adapterUtils.assertAdapterValueSelected( element, 0, content ) );
+        } );
+
+        it( 'reset elements initial value', function () {
+            cy.get( selector )
+                    .type( content )
+                    .then( ( element ) => {
+                        adapterUtils.assertAdapterHoldsAmountOfValues( element, 1 );
+                        adapterUtils.assertAdapterValueSelected( element, 0, content );
+                        adapterUtils.getAdapterForElement( element ).reset();
+                        adapterUtils.assertAdapterValueSelected( element, 0, '' );
+                        adapterUtils.assertAdapterHoldsAmountOfValues( element, 1 );
+                    } )
+        } );
+
+        it( 'bootstrapui.change is fired when a change event is emitted on the element', function () {
+            cy.get( selector )
+                    .then( ( element ) => {
+                        adapterUtils.assertThatBootstrapUiChangeIsTriggeredOn( element, 'change' );
+                    } )
+        } );
+    };
+
+    before( function () {
+        cy.visit( "/control-adapters" );
+    } );
+
+    describe( 'textbox', () => {
+        const content = 'Winter wonderland';
+        basicAdapterTests( '#ca-textbox', content );
+    } );
+
+    describe( 'autosizing textbox', () => {
+        const content = "my life got flipped turned upside down and I'd like to take a minute just sit right there";
+        basicAdapterTests( '#ca-textbox-autosize', content );
+    } );
+
+    describe( 'textarea', () => {
+        const content = "Festive feasts\n\n\n\n\n!!";
+        basicAdapterTests( '#ca-textarea', content );
+    } );
+
+    describe( 'numeric without format', () => {
+        const content = '1234';
+        basicAdapterTests( '#ca-numeric-noformat', content );
+    } );
+} );
