cccf640a8a423b670fafde7a2cb7d1cc90f1d7d7
Simon VDB <simonvdbroeck@gmail.com>
2019-08-09 08:31:35 +0200

    linting of e2e files

--- bootstrap-ui-module-frontend/test/e2e/tests/cypress/support/utils/control-adapters.js
+++ bootstrap-ui-module-frontend/test/e2e/tests/cypress/support/utils/control-adapters.js
@@ -20,122 +20,124 @@
  * @type {{getAdapterForElement: (function(*): *), getAdaptersOfType: (function(*): Cypress.Chainable<JQuery<HTMLElement>>), assertAdapterValueSelected: Cypress.Commands.ControlAdapter.assertAdapterValueSelected, assertAdapterNoValueSelected: Cypress.Commands.ControlAdapter.assertAdapterNoValueSelected}}
  */
 export default {
-    /**
-     * Returns the adapter for a given jquery node that was selected via {@code cy}.
-     *
-     * @param jqueryNode the node for which to fetch the adapter.
-     * @returns {*}
-     */
-    getAdapterForElement: function ( jqueryNode ) {
-        return jqueryNode.data( 'bootstrapui-adapter' );
-    },
-    /**
-     * Selects all elements of a given adapter type.
-     *
-     * @see cy#get
-     * @param type
-     * @returns {Cypress.Chainable<JQuery<HTMLElement>>}
-     */
-    getAdaptersOfType: function ( type ) {
-        return cy.get( `[data-bootstrapui-adapter-type='${type}']` );
-    },
-    /**
-     * Checks whether an adapter exists on a specific element.
-     *
-     * @param selector for the element to check
-     */
-    assertThatAdapterExists: function ( selector ) {
-        cy.get( selector )
-                .then( ( element ) => {
-                    expect( element.data( 'bootstrapui-adapter' ) ).to.not.be.undefined;
-                } );
-    },
-    /**
-     * Checks whether an element has underlying control adapter elements.
-     *
-     * @param selector of the element to check
-     * @param amountOfControlAdapters that should be present
-     */
-    assertHasUnderlyingControlAdapters: function ( selector, amountOfControlAdapters ) {
-        cy.get( selector )
-                .then( ( item ) => {
-                    expect( item.find( '[data-bootstrapui-adapter-type]' ) ).to.have.property( 'length', amountOfControlAdapters || 0 );
-                } );
-    },
-    /**
-     * Validates whether an adapter holds a specific value.
-     *
-     * @param element jquery node on which the adapter is registered
-     * @param index  of the value to check (optional - default 0)
-     * @param label value that should be present as label (optional)
-     * @param value that should be present as value (optional)
-     * @param context node that defines the value (optional)
-     */
-    assertAdapterValueSelected: function ( element, index, label, value, context ) {
-        const adapter = this.getAdapterForElement( element );
-        const valueToCheck = adapter.getValue()[index || 0];
-        if ( label !== undefined ) {
-            expect( valueToCheck ).to.have.property( 'label', label );
-        }
-        if ( value !== undefined ) {
-            expect( valueToCheck ).to.have.property( 'value', value );
-        }
-        if ( context !== undefined ) {
-            expect( valueToCheck ).to.have.property( 'context', context );
-        }
-    },
-    /**
-     * Checks whether an element currently holds a certain amount of values.
-     *
-     * @param element on which the adapter is registered
-     * @param amountOfValues to expect when calling {@link BootstrapUiControlAdapter#getValue}
-     */
-    assertAdapterHoldsAmountOfValues: function ( element, amountOfValues ) {
-        const adapter = this.getAdapterForElement( element );
-        expect( adapter.getValue() ).to.have.length( amountOfValues );
-    },
-    /**
-     * Validates that the adapter holds no values.
-     *
-     * @param element jquery node on which the adapter is registered
-     */
-    assertAdapterNoValueSelected: function ( element ) {
-        const adapter = this.getAdapterForElement( element );
-        expect( adapter.getValue() ).to.have.length( 0 );
-    },
-    /**
-     * Checks whether the {@code bootstrapui.change} event is triggered when the specified event is executed
-     *
-     * @param element that holds the adapter
-     * @param event name of the event that should trigger {@code bootstrapui.change}
-     * @returns {*}
-     */
-    assertThatBootstrapUiChangeIsTriggeredOn: function ( element, event ) {
-        const adapter = this.getAdapterForElement( element );
+  /**
+   * Returns the adapter for a given jquery node that was selected via {@code cy}.
+   *
+   * @param jqueryNode the node for which to fetch the adapter.
+   * @returns {*}
+   */
+  getAdapterForElement: function(jqueryNode) {
+    return jqueryNode.data('bootstrapui-adapter');
+  },
+  /**
+   * Selects all elements of a given adapter type.
+   *
+   * @see cy#get
+   * @param type
+   * @returns {Cypress.Chainable<JQuery<HTMLElement>>}
+   */
+  getAdaptersOfType: function(type) {
+    return cy.get(`[data-bootstrapui-adapter-type='${type}']`);
+  },
+  /**
+   * Checks whether an adapter exists on a specific element.
+   *
+   * @param selector for the element to check
+   */
+  assertThatAdapterExists: function(selector) {
+    cy.get(selector).then(element => {
+      expect(element.data('bootstrapui-adapter')).to.not.be.undefined;
+    });
+  },
+  /**
+   * Checks whether an element has underlying control adapter elements.
+   *
+   * @param selector of the element to check
+   * @param amountOfControlAdapters that should be present
+   */
+  assertHasUnderlyingControlAdapters: function(
+    selector,
+    amountOfControlAdapters
+  ) {
+    cy.get(selector).then(item => {
+      expect(item.find('[data-bootstrapui-adapter-type]')).to.have.property(
+        'length',
+        amountOfControlAdapters || 0
+      );
+    });
+  },
+  /**
+   * Validates whether an adapter holds a specific value.
+   *
+   * @param element jquery node on which the adapter is registered
+   * @param index  of the value to check (optional - default 0)
+   * @param label value that should be present as label (optional)
+   * @param value that should be present as value (optional)
+   * @param context node that defines the value (optional)
+   */
+  assertAdapterValueSelected: function(element, index, label, value, context) {
+    const adapter = this.getAdapterForElement(element);
+    const valueToCheck = adapter.getValue()[index || 0];
+    if (label !== undefined) {
+      expect(valueToCheck).to.have.property('label', label);
+    }
+    if (value !== undefined) {
+      expect(valueToCheck).to.have.property('value', value);
+    }
+    if (context !== undefined) {
+      expect(valueToCheck).to.have.property('context', context);
+    }
+  },
+  /**
+   * Checks whether an element currently holds a certain amount of values.
+   *
+   * @param element on which the adapter is registered
+   * @param amountOfValues to expect when calling {@link BootstrapUiControlAdapter#getValue}
+   */
+  assertAdapterHoldsAmountOfValues: function(element, amountOfValues) {
+    const adapter = this.getAdapterForElement(element);
+    expect(adapter.getValue()).to.have.length(amountOfValues);
+  },
+  /**
+   * Validates that the adapter holds no values.
+   *
+   * @param element jquery node on which the adapter is registered
+   */
+  assertAdapterNoValueSelected: function(element) {
+    const adapter = this.getAdapterForElement(element);
+    expect(adapter.getValue()).to.have.length(0);
+  },
+  /**
+   * Checks whether the {@code bootstrapui.change} event is triggered when the specified event is executed
+   *
+   * @param element that holds the adapter
+   * @param event name of the event that should trigger {@code bootstrapui.change}
+   * @returns {*}
+   */
+  assertThatBootstrapUiChangeIsTriggeredOn: function(element, event) {
+    const adapter = this.getAdapterForElement(element);
 
-        let targetNode;
-        if ( element.is( adapter.getTarget() ) ) {
-            targetNode = element;
-        }
-        else {
-            targetNode = element.find( adapter.getTarget() );
-        }
-
-        const obj = {
-            handle( controlAdapter ) {
-                return controlAdapter;
-            }
-        };
-        const spy = cy.spy( obj, 'handle' );
-
-        element.on( "bootstrapui.change", function ( event, controlAdapter ) {
-            obj.handle( controlAdapter );
-        } );
-
-        targetNode.trigger( event );
-
-        expect( spy ).to.have.callCount( 1 );
-        expect( spy ).to.have.returned( adapter );
+    let targetNode;
+    if (element.is(adapter.getTarget())) {
+      targetNode = element;
+    } else {
+      targetNode = element.find(adapter.getTarget());
     }
 
+    const obj = {
+      handle(controlAdapter) {
+        return controlAdapter;
+      },
+    };
+    const spy = cy.spy(obj, 'handle');
+
+    element.on('bootstrapui.change', function(event, controlAdapter) {
+      obj.handle(controlAdapter);
+    });
+
+    targetNode.trigger(event);
+
+    expect(spy).to.have.callCount(1);
+    expect(spy).to.have.returned(adapter);
+  },
 };
