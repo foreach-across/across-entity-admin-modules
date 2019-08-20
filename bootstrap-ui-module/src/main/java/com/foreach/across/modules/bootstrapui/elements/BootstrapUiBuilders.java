958964011d905be8d0f11a8b44463af0cd7a7d71
Arne Vandamme <arne@foreach.be>
2019-08-20 20:56:24 +0200

    Add Card example controller

--- bootstrap-ui-module/src/main/java/com/foreach/across/modules/bootstrapui/elements/BootstrapUiBuilders.java
+++ bootstrap-ui-module/src/main/java/com/foreach/across/modules/bootstrapui/elements/BootstrapUiBuilders.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 the original author or authors
+ * Copyright 2019 the original author or authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -90,8 +90,8 @@ public class BootstrapUiBuilders
 		return FACTORY.paragraph();
 	}
 
-	public static LinkViewElementBuilder link() {
-		return FACTORY.link();
+	public static LinkViewElementBuilder link( ViewElement.WitherSetter... setters ) {
+		return FACTORY.link().with( setters );
 	}
 
 	public static FormViewElementBuilder form() {
