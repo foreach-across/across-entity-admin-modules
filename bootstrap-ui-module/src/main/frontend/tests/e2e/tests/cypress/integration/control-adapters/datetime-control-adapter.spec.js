712416c959df1a22ceae489d62bcc0e643ada227
Steven Gentens <stg@foreach.be>
2019-09-11 16:50:58 +0200

    remove superfluous code in datetime control adapter tests

--- bootstrap-ui-module/src/main/frontend/tests/e2e/tests/cypress/integration/control-adapters/datetime-control-adapter.spec.js
+++ bootstrap-ui-module/src/main/frontend/tests/e2e/tests/cypress/integration/control-adapters/datetime-control-adapter.spec.js
@@ -21,8 +21,6 @@ describe( 'ControlAdapter - Datepicker', function () {
 
     before( function () {
         cy.visit( "/utilities/control-adapters" );
-        cy.get( 'li.active > a' )
-                .click();
     } );
 
     it( "adapter exists", function () {
