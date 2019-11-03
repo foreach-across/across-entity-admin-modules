c89a884ec61ce0bc4968ce3a8d33a51d1a02e736
Arne Vandamme <arne@foreach.be>
2019-11-03 20:37:58 +0100

    BootstrapUiModule now extends AcrossWebModule

--- bootstrap-ui-module/src/main/java/com/foreach/across/modules/bootstrapui/BootstrapUiModule.java
+++ bootstrap-ui-module/src/main/java/com/foreach/across/modules/bootstrapui/BootstrapUiModule.java
@@ -1,7 +1,25 @@
+/*
+ * Copyright 2019 the original author or authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.foreach.across.modules.bootstrapui;
 
 import com.foreach.across.core.AcrossModule;
 import com.foreach.across.core.annotations.AcrossDepends;
+import com.foreach.across.core.context.bootstrap.AcrossBootstrapConfig;
+import com.foreach.across.core.context.bootstrap.ModuleBootstrapConfig;
 import com.foreach.across.core.context.configurer.ApplicationContextConfigurer;
 import com.foreach.across.core.context.configurer.ComponentScanConfigurer;
 import com.foreach.across.modules.web.AcrossWebModule;
@@ -20,11 +38,16 @@ public class BootstrapUiModule extends AcrossModule
 
 	@Override
 	public String getDescription() {
-		return "Provides infrastructure and components for building a Bootstrap based web interface.";
+		return "Extension module: extends AcrossWebModule with infrastructure for building a Bootstrap based web interface.";
 	}
 
 	@Override
 	protected void registerDefaultApplicationContextConfigurers( Set<ApplicationContextConfigurer> contextConfigurers ) {
-		contextConfigurers.add( new ComponentScanConfigurer( getClass().getPackage().getName() + ".config" ) );
+		// only extend AcrossWebModule
+	}
+
+	@Override
+	public void prepareForBootstrap( ModuleBootstrapConfig currentModule, AcrossBootstrapConfig contextConfig ) {
+		contextConfig.extendModule( AcrossWebModule.NAME, new ComponentScanConfigurer( getClass().getPackage().getName() + ".config" ) );
 	}
 }
