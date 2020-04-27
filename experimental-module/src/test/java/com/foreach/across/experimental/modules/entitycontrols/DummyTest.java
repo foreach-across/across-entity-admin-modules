a239094c88f8b7f586303714f3735ee2ee41d629
stijn <vanhoof.stijn@gmail.com>
2020-04-27 11:05:28 +0200

    Dummy test added

---
+++ experimental-module/src/test/java/com/foreach/across/experimental/modules/entitycontrols/DummyTest.java
@@ -0,0 +1,12 @@
+package com.foreach.across.experimental.modules.entitycontrols;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+public class DummyTest {
+    @Test
+    public void dummyTest() {
+        assertThat(true).isTrue();
+    }
+}
