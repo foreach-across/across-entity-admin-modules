1e7b8fdc87d63c0998902be25c77ad827f2ffa78
stijn <vanhoof.stijn@gmail.com>
2020-04-27 10:05:00 +0200

    Add adminweb module for testing

--- experimental-test-application/src/main/java/com/foreach/across/experiments/ExperimentalModuleTestApplication.java
+++ experimental-test-application/src/main/java/com/foreach/across/experiments/ExperimentalModuleTestApplication.java
@@ -3,11 +3,10 @@ package com.foreach.across.experiments;
 import com.foreach.across.AcrossApplicationRunner;
 import com.foreach.across.config.AcrossApplication;
 import com.foreach.across.experimental.ExperimentalModule;
-import com.foreach.across.modules.adminweb.AdminWebModule;
 import com.foreach.across.modules.entity.EntityModule;
 import org.springframework.context.annotation.Bean;
 
-@AcrossApplication(modules = {AdminWebModule.NAME, EntityModule.NAME})
+@AcrossApplication(modules = {EntityModule.NAME})
 public class ExperimentalModuleTestApplication {
     @Bean
     public ExperimentalModule experimentalModule() {
