dae2862f41dcce9dae1ba6c9808dfcd8af75ad22
Steven Gentens <stg@foreach.be>
2020-09-30 11:42:16 +0200

    Added WebUtilityModule documentation for actions - updated RequestActionHandlerAttribute javadoc to represent its current state.

--- experimental-module/src/main/java/com/foreach/across/modules/experimental/webutility/support/action/RequestActionHandlerAttribute.java
+++ experimental-module/src/main/java/com/foreach/across/modules/experimental/webutility/support/action/RequestActionHandlerAttribute.java
@@ -2,10 +2,6 @@ package com.foreach.across.modules.experimental.webutility.support.action;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.foreach.across.modules.entity.views.ViewElementMode;
-import com.foreach.across.modules.web.ui.ViewElement;
-import com.foreach.across.modules.web.ui.ViewElementPostProcessor;
-import com.foreach.across.modules.web.ui.elements.HtmlViewElement;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
@@ -14,37 +10,9 @@ import org.springframework.http.HttpMethod;
 import java.util.Map;
 
 /**
- * Enable automatic partial snippet refreshing on an element.
- * <p/>
- * This will add a {@code data-partial-load} html attribute to the html node, which will trigger
- * a client-side (AJAX) request based on a control value change. Works by default with all elements
- * that publish a {@code bootstrapui.change} event (which is all default bootstrap form controls).
- * <p/>
- * Which request is executed and where the resulting output is sent to depends on the value of the different
- * properties. Read the javadoc below for more details and see also the {@code partial-loader.js} javascript
- * extension for the client-side code.
- * <p/>
- * This configurer can either be used with {@link HtmlViewElement#set(ViewElement.WitherSetter[])},
- * as a {@link ViewElementPostProcessor} or for automatic {@link com.foreach.across.modules.entity.registry.properties.EntityPropertyDescriptor}
- * configuration to activate partial refresh for the {@link ViewElementMode#CONTROL}.
- * <p/>
- * Example usage:
- * <pre>{@code
- *  .properties( props -> props.property( "productFeatureType" )
- *                             .viewElementPostProcessor( ViewElementMode.FILTER_CONTROL,
- * 				                                          refreshPartial( "itemsTable" )
- * 						                                           .target( ".em-sortableTable-panel" )
- * 						                                           .form( "form" )
- * 						                                           .url(  entityViewLinks.linkTo(ProductFeatureValue.class ) ) )
- * )
- * }</pre>
- * <p/>
- * At minimum a partial name is required. If it is the only thing specified (neither {@link #form(String)} nor {@link #url(String)} is set,
- * the form closest to the control will be fetched and its {@code action} will be used as the target url (but using the {@link #method(HttpMethod)}
- * that was configured. If both form and url are set, only the form data of the form will be serialized and sent to the target url.
- * <p/>
- * If no {@link #target(String)} is specified, a target element with attribute {@code data-partial-target=NAME_OF_THE_PARTIAL} will be updated.
- * If it also does not exist, the original control will be replaced instead. As soon as a target is specified, it will be used instead.
+ * {@link ActionHandlerAttribute} that supports refreshing fetching and replacing an element.
+ * This will trigger a client-side request based on the configured properties.
+ * When the request is resolved, the content of the response will replace the {@link #target} element.
  */
 @Setter
 @Accessors(chain = true, fluent = true)
@@ -89,8 +57,7 @@ public class RequestActionHandlerAttribute extends ActionHandlerAttribute<Reques
 
 	/**
 	 * CSS3 selector of the element that should be replaced by the partial output.
-	 * If the target element has an attribute {@code data-partial-target}, only the content of the target will be
-	 * replaced. Else the target element in its entirety will be replaced by the partial output.
+	 * The target element in its entirety will be replaced by the partial output.
 	 */
 	@JsonProperty
 	private String target;
