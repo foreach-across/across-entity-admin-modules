64785fea60544648742864f9c980e267b7dac808
Marc Vanbrabant <marc@foreach.be>
2020-11-30 15:24:42 +0100

    Use intercept method for waiting on fetch dialogs

--- experimental-test-application/src/test/resources/e2e/cypress/integration/bulk.js
+++ experimental-test-application/src/test/resources/e2e/cypress/integration/bulk.js
@@ -4,12 +4,27 @@ context('Depends On tests', () => {
 
     beforeEach(() => {
         cy.login('admin')
+
+        // Cypress.minimatch('/users/1/comments/2', '/users/*/comments', { matchBase: true })
+        cy.intercept('GET', '/admin/entities/food/create?_partial=content', (req) => {
+            console.log("modal is loading...")
+        }).as('ajaxModalGet');
+        cy.intercept('POST', '/admin/entities/food/create?_partial=::body', (req) => {
+            console.log("food is being saved")
+        }).as('ajaxModalPost');
+        cy.intercept('GET', '/admin/entities/food?_partial=::itemsTable', (req) => {
+            console.log("list is being refreshed")
+        }).as('ajaxEntitiesList');
         cy.server();
-        cy.route('GET', '/admin/entities/food/**').as("ajaxModalGet");
-        cy.route('POST', '/admin/entities/food/**').as("ajaxModalPost");
+        cy.intercept('GET', new RegExp("\\/admin\\/entities\\/food\\/.*\\/delete\\?_partial=content"), (req) => {
+            console.log("delete modal loading...")
+        }).as('ajaxModalDelete');
+        cy.intercept('POST', new RegExp("\\/admin\\/entities\\/food\\/.*\\/delete\\?_partial=::body"), (req) => {
+            console.log("delete is completed")
+        }).as('ajaxModalDeleted');
     });
 
-    it('do not show percentage when it does not contains alcohol', function () {
+    it('Create a new food using a modal', function () {
         cy.goToMenuItem('ExperimentalModuleTestApplicationModule').goToMenuItem('Food');
 
         cy.contains("Create a new food").click();
@@ -19,6 +34,7 @@ context('Depends On tests', () => {
 
         //cy.contains( ".modal-content", "Name").type(food);
         cy.wait("@ajaxModalGet");
+        cy.get(".modal-content").should("be.visible");
 
         //cy.get( "textarea").type(food);
         cy.contains("Save").click();
@@ -26,9 +42,14 @@ context('Depends On tests', () => {
         cy.contains("Unable to save, please check the form for one or more errors");
         cy.get("textarea").type(food);
         cy.contains("Save").click();
-        cy.wait("@ajaxModalPost")
-        //cy.contains("Cancel").click();
 
+        cy.wait("@ajaxEntitiesList");
+        cy.get(".modal-content").should("not.be.visible");
+
+        cy.contains(food).next().should('have.value', '').next().find('.fa-times').click();
+        cy.wait("@ajaxModalDelete");
+        cy.contains('button', 'Delete').click();
+        cy.wait("@ajaxEntitiesList");
     });
 
 });
