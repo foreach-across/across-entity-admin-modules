131e3b913dbeb9c4c494a158addd488958cdf718
Marc Vanbrabant <marc@foreach.be>
2020-11-30 11:22:34 +0100

    First tests for depends on

---
+++ experimental-test-application/src/test/resources/e2e/cypress/support/commands.js
@@ -0,0 +1,21 @@
+Cypress.Commands.add("login", (username) => {
+    cy.fixture("users").then(users => {
+        cy.visit("/admin/login");
+
+        cy.get('#username')
+            .clear()
+            .type(username)
+            .get('#password')
+            .clear()
+            .type(users[username])
+            .get('.btn-primary')
+            .click();
+    });
+
+});
+
+Cypress.Commands.add('goToMenuItem', (title) => {
+    cy.get('.sidebar').within(() => {
+        cy.get('a[title="' + title + '"]').click();
+    });
+});
