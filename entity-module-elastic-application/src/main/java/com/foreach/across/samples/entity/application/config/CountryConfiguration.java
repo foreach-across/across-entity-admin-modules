119847a577f1af16fed48abcb398ddfcd5b8a680
Steven Gentens <stg@foreach.be>
2021-02-03 23:53:02 +0100

    elasticsearch wip association view configuration

--- entity-module-elastic-application/src/main/java/com/foreach/across/samples/entity/application/config/CountryConfiguration.java
+++ entity-module-elastic-application/src/main/java/com/foreach/across/samples/entity/application/config/CountryConfiguration.java
@@ -16,21 +16,34 @@
 
 package com.foreach.across.samples.entity.application.config;
 
+import com.foreach.across.core.annotations.Module;
+import com.foreach.across.core.context.info.AcrossModuleInfo;
+import com.foreach.across.modules.entity.EntityModule;
 import com.foreach.across.modules.entity.config.EntityConfigurer;
 import com.foreach.across.modules.entity.config.builders.EntitiesConfigurationBuilder;
+import com.foreach.across.modules.entity.config.builders.EntityConfigurationBuilder;
 import com.foreach.across.modules.entity.registry.EntityAssociation;
+import com.foreach.across.modules.entity.registry.MutableEntityConfiguration;
+import com.foreach.across.modules.entity.views.DispatchingEntityViewFactory;
 import com.foreach.across.modules.entity.views.context.ConfigurableEntityViewContext;
 import com.foreach.across.modules.entity.views.context.EntityViewContext;
 import com.foreach.across.modules.entity.views.menu.EntityAdminMenuEvent;
 import com.foreach.across.modules.entity.views.processors.EntityViewProcessorAdapter;
+import com.foreach.across.modules.entity.views.processors.support.EntityViewProcessorRegistry;
 import com.foreach.across.modules.entity.web.links.EntityViewLinkBuilder;
 import com.foreach.across.testmodules.elastic.config.EntityElasticsearchConfiguration;
 import com.foreach.across.testmodules.elastic.domain.elastic.country.ElasticCountry;
 import com.foreach.across.testmodules.elastic.domain.elastic.customer.ElasticCustomer;
 import com.foreach.across.testmodules.elastic.domain.jpa.country.Country;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.EventListener;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Objects;
+import java.util.function.Consumer;
 
 /**
  * WIP configuring view navigation within associations.
@@ -39,37 +52,71 @@ import org.springframework.context.event.EventListener;
 @Configuration
 public class CountryConfiguration implements EntityConfigurer
 {
+
+	private final AcrossModuleInfo moduleInfo;
+
+	public CountryConfiguration( @Module(EntityModule.NAME) AcrossModuleInfo moduleInfo ) {
+		this.moduleInfo = moduleInfo;
+	}
+
 	@Override
 	public void configure( EntitiesConfigurationBuilder entities ) {
 		entities.withType( ElasticCountry.class )
 		        .attribute( EntityElasticsearchConfiguration.ATTR_ELASTIC_PROXY_REFERENCE, Country.class );
 
 		entities.withType( Country.class )
-		        .association(
-				        ab -> ab.name( "customer.country" ).associationType( EntityAssociation.Type.EMBEDDED )
-				                .createOrUpdateFormView(
-						                fvb -> fvb.viewProcessor(
-								                vp -> vp.provideBean( new ElasticAssociationViewContextLinkBuilderProcessor( "elasticCustomer.country",
-								                                                                                             "customer.country" ) ) )
-				                )
-				                .deleteFormView(
-						                fvb -> fvb.viewProcessor(
-								                vp -> vp.provideBean( new ElasticAssociationViewContextLinkBuilderProcessor( "elasticCustomer.country",
-								                                                                                             "customer.country" ) ) )
-				                )
-		        )
-		        .association(
-				        ab -> ab.name( "elasticCustomer.country" )
-				                .targetEntityType( ElasticCustomer.class )
-				                .sourceProperty( "id" )
-				                .targetProperty( "country.id" )
-				                .associationType( EntityAssociation.Type.EMBEDDED )
-				                .listView(
-						                lvb -> lvb.viewProcessor(
-								                vp -> vp.provideBean( new ElasticAssociationViewContextLinkBuilderProcessor( "elasticCustomer.country",
-								                                                                                             "customer.country" ) ) )
-				                )
-		        );
+		        .association( ab -> ab.name( "customer.country" ).associationType( EntityAssociation.Type.EMBEDDED ) )
+		        .postProcessor( configureElasticSearchAssociationQuery( "customer.country",
+		                                                                "elasticCustomer.country",
+		                                                                ElasticCustomer.class, "id",
+		                                                                "country.id" ) );
+	}
+
+	private Consumer<MutableEntityConfiguration<Country>> configureElasticSearchAssociationQuery( String targetAssociation,
+	                                                                                              String searchAssociation,
+	                                                                                              Class<ElasticCustomer> searchEntityType,
+	                                                                                              String targetAssociationProperty,
+	                                                                                              String matchingSearchEntityProperty ) {
+		return mec -> {
+			Collection<EntityAssociation> associations = mec.getAssociations();
+			associations.stream()
+			            .filter( ass -> StringUtils.equalsIgnoreCase( targetAssociation, ass.getName() ) )
+			            .findFirst()
+			            .ifPresent(
+					            ass -> {
+						            Arrays.stream( mec.getViewNames() )
+						                  .filter( vn -> StringUtils.startsWith( vn, targetAssociation ) )
+						                  .map( mec::getViewFactory )
+						                  .filter( Objects::nonNull )
+						                  .filter( vf -> DispatchingEntityViewFactory.class.isAssignableFrom( vf.getClass() ) )
+						                  .map( DispatchingEntityViewFactory.class::cast )
+						                  .forEach(
+								                  dvf -> {
+									                  EntityViewProcessorRegistry processorRegistry = dvf.getProcessorRegistry();
+									                  if ( !processorRegistry.contains( ElasticAssociationViewContextLinkBuilderProcessor.class.getName() ) ) {
+										                  processorRegistry.addProcessor(
+												                  new ElasticAssociationViewContextLinkBuilderProcessor( searchAssociation, targetAssociation )
+										                  );
+									                  }
+								                  }
+						                  );
+					            }
+			            );
+
+			new EntityConfigurationBuilder<Country>( moduleInfo.getApplicationContext().getAutowireCapableBeanFactory() )
+					.association(
+							ab -> ab.name( searchAssociation )
+							        .targetEntityType( searchEntityType )
+							        .sourceProperty( targetAssociationProperty )
+							        .targetProperty( matchingSearchEntityProperty )
+							        .associationType( EntityAssociation.Type.EMBEDDED )
+							        .listView( lvb -> lvb.viewProcessor(
+									        vp -> vp.provideBean(
+											        new ElasticAssociationViewContextLinkBuilderProcessor( searchAssociation, targetAssociation ) )
+							        ) ).show()
+					)
+					.apply( mec );
+		};
 	}
 
 	@EventListener
